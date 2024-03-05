5c18d93ce4ad66b78585af58b0818a2a21528c57
Caesar99-byte <caesar99cagb@gmail.com>
2024-03-05 14:29:34 -0600

    Tarea de Interface

--- global.js
+++ global.js
@@ -1,5 +1,9 @@
-document.querySelector(".close").addEventListener("click",function(){
-
+document.querySelector(".close").addEventListener("click", function(){
     console.log("click");
-    document.querySelector(".ventana-open").classList.remove("open");
-}
+    document.querySelector(".ventana").classList.remove("open");
+});
+
+document.querySelector(".open-button").addEventListener("click", function(){
+    console.log("click");
+    document.querySelector(".ventana").classList.add("open");
+});
